d1a8876bb8d1125474066be191a567788b8ccee0
MathiasHun1 <141583942+MathiasHun1@users.noreply.github.com>
2024-08-08 17:26:56 +0200

    made changes for production

--- groceries_BE/app.js
+++ groceries_BE/app.js
@@ -8,7 +8,12 @@ const mongoose = require('mongoose')
 const { URI } = require('./utils/config')
 const cors = require('cors')
 const { tokenExtractor, userExtractor, errorHandler, requestLogger } = require('./utils/middleware')
+const path = require('path')
+
 require('dotenv').config()
+
+const frontend = path.join(__dirname, 'dist')
+
 
 console.log('Connecting MongoDB..')
 mongoose.connect(URI)
@@ -22,10 +27,14 @@ if(process.env.NODE_ENV !== 'production') {
 }
 app.use(tokenExtractor)
 app.use(userExtractor)
+
+app.use(express.static('dist'))
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/register', regRouter)
+app.use('*', express.static(frontend))
+
 app.use(errorHandler)
 
 module.exports = app
